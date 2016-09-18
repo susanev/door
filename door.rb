3cd2c10d6a9a1170391da32e97ab44b7d00167e4
Trish Griego <pmgriego@gmail.com>
2016-09-18 10:51:00 -0700

    Created basic class object trying to be thoughtful in my instatiation.

---
+++ door.rb
@@ -0,0 +1,18 @@
+#door.rb
+
+class Door
+
+  # - display an inscription
+  # - be either open or closed, and
+  # - be either locked or unlocked
+  # could I use different naming convention. Of using lockedness?, openess?
+  # coded as is, they could be booleans.
+
+  def initialize(display, openess, lockedness)
+    @display = display
+    @openesss = openesss
+    @locked = locked 
+
+  end
+
+end
