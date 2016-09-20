d0abc4ac6203681d757894789d76a5cbc00beb20
Trish Griego <pmgriego@gmail.com>
2016-09-20 14:21:16 -0700

    Planned out logic for OpenDoor, ClosedDoor, and LockedDoor states. Just finished writing a open method.

--- door.rb
+++ door.rb
@@ -2,49 +2,83 @@
 
 class Door
 
-  # - display an inscription
-  # - be either open or closed, and
-  # - be either locked or unlocked
-
-
-  ### Q: could I use different naming convention. Of using lockedness?, openess?
-  # coded as is, they could be booleans.
-  ### Q: could I instantiate amount of descriptions used? Like set description_amount = 1.
-  # attr_accessor :decriptions, : is_open,
-
 # not sure how to deal with this yet: description
 
-  def initialize(door_open, door_locked)
+  def initialize(is_door_open, is_door_locked)
     # @description = description
-    @door_open = door_open
-    @door_locked = door_locked
-  end
-
-  def is_closed?()
-
-  end
-
-  def is_locked?()
-
+    @is_door_open = is_door_open
+    @is_door_locked = is_door_locked
   end
 
 # 4 states that could happen to a door object, typically by human or user.
 # the above methods are tests for above.
 
   def open()
+    # so if the door is closed , door is unlocked
+    if @is_door_open == false && @is_door_locked == false
+       @is_door_open = true
+    elsif
+      # door is already open
+      @is_door_open == true
+       puts "door is already open... trish, you will need to place an error here."
+    else
+      # so if door is not open, && is locked, you cannot open it.
+      @is_door_open == false && @is_door_locked == true
+      puts "You cannot open a locked door, fool!"
+    end
 
   end
-
-  def close()
-
-  end
-
-  def lock()
-
-  end
-
-  def unlock()
-
-  end
+  #
+  # def close()
+  #
+  # end
+  #
+  # def lock()
+  #
+  # end
+  #
+  # def unlock()
+  #
+  # end
 
 end
+
+
+
+# created a test, to see that if the door is closed (as in not opened), and that its unlocked it can be opened.
+# I expect the outcome to be true.
+# test = Door.new(false, false)
+# puts test.open
+# expected it to return true. 
+
+#created 2nd test, to see if the door is open, can I open it?
+# test2 = Door.new(true, false)
+# puts test2.open
+# expect it to be false. As you cannot open an open door. Actually, printed the message,"door is already open...trish, you will need to place an error here."
+
+# Created 2nd test, to see if the door was open, and locked.
+test3 = Door.new(false, true)
+puts test3.open
+# expect it would be false, or display a message that says,"you cannot open a locked door."
+
+
+
+
+
+## OpenDoor ##
+  ## cannot be opened
+  ## can be closed
+  ## cannot be locked
+  ## cannot be unlocked
+
+## ClosedDoor ##
+  ## can be opened
+  ## cannnot be closed
+  ## can be locked
+  ## cannot be unlocked
+
+## LockedDoor ##
+  ## cannot be opned
+  ## cannot be closed
+  ## cannot be locked
+  ## can be unlocked
