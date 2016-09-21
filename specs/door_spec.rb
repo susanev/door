16a594b9883578a87cecd1246b79a291ea235403
Trish Griego <pmgriego@gmail.com>
2016-09-20 17:14:58 -0700

    Linked and created a Rakefile and Spec_helper.rb file

---
+++ specs/door_spec.rb
@@ -0,0 +1,16 @@
+# door_spec.rb
+require_relative 'Spec_helper'
+require_relative '../door'
+
+# require_relative 'Spec_helper'
+#
+# require_relative ''
+
+# describe Door do
+#   it " " do
+#
+#   end
+#
+#
+#
+# end
