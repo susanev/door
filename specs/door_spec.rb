786113f78c3e53229b763da2f1c2e006958f6e18
Trish Griego <pmgriego@gmail.com>
2016-09-20 17:57:05 -0700

    Finished writing tests for the rest of the possible door states. Tests are that aren't able to run are commented out. Will turn them on again, once I've written those methods.

--- specs/door_spec.rb
+++ specs/door_spec.rb
@@ -4,14 +4,14 @@ require_relative '../door'
 
 describe Door do
 
-## OpenDoor tests ##
+################### OpenDoor tests ###################
 
   ## cannot be opened
   ## can be closed
   ## cannot be locked
   ## cannot be unlocked
 
-#test1
+#01
   it "Test to see if open door, cannot be opened (params: true, false)" do
     # as in, is_door_open = true, is_door_locked = false
     # door is open, its not locked.
@@ -19,55 +19,89 @@ describe Door do
     expect(door.open).must_equal(nil)
   end
 
-#test2
+#02
   it "test, to see that if the door is closed (as in not opened), and that its unlocked it can be opened." do
-    # as in, is_door_open = fale and is_door_locked = false
+    # as in, is_door_open = false and is_door_locked = false
     # door is closed, and unlocked.
     door = Door.new(false, false)
     expect(door.open).must_equal(true)
-
   end
-#test3
-#door cannot be locked.
-it "Test to see if open door cannot be locked. Should return nil(?)"
-door = Door.new(true, true)
-expect(door.locked).must_equal(nil)
-# or argument error.
+#
+# #03
+#   #door cannot be locked.
+#   it "Test to see if open door cannot be locked. Should return nil(?)"
+#   door = Door.new(true, true)
+#   expect(door.locked).must_equal(nil)
+#   # or argument error.
+#   end
+#
+# #04
+#   #open door cannot be unlocked.
+#   it "Test to see if an open door cannot be unlocked."
+#   door = Door.new(true, true)
+#   expect(door.unlock).must_equal(nil)
+#   # or raise an argument error.
+#   end
 
-#test4
-#open door cannot be unlocked.
-it "Test to see if an open door cannot be unlocked."
-door = Door.new(true, true)
-expect(door.unlock).must_equal(nil )
-# or raise an argument error.
-end
-
-## ClosedDoor tests ##
+################### ClosedDoor tests ###################
   ## can be opened
   ## cannnot be closed
   ## can be locked
   ## cannot be unlocked
+#
+# #05
+#   it "Test to see if a closed door, can be opened" do
+#     door = Door.new(false, true)
+#     expect(door.open).must_equal(true)
+#   end
+#
+# #06
+#   it "Test to see if a closed door, CANNOT be closed." do
+#     door = Door.new(false, true)
+#     expect(door.close).must_equal(nil)
+#     # eventually, raise argument error?
+#   end
+#
+# #07
+#   it "Test to see if a closed door, can be locked " do
+#     door = Door.new(false, false)
+#     expect(door.locked).must_equal(nil)
+#   end
+# #08
+#   it "Test to see if a closed door, CANNOT be unlocked " do
+#     door = Door.new(false, false)
+#     expect(door.unlock).must_equal(nil)
+#   end
 
-#test5
-  it "Test to see if a closed door, can be opened" do
-    door = Door.new(true, false)
-    expect(door.open).must_equal(nil)
-  end
-#test6
-  it "Test to see if a closed door, CANNOT be closed." do
-    door = Door.new(true, false)
-    expect(door.open).must_equal(nil)
-  end
-#test7
-  it "Test to see if a closed door, can be locked " do
-    door = Door.new(true, false)
-    expect(door.open).must_equal(nil)
-  end
-#test 8
-  it "Test to see if a closed door, CANNOT be unlocked " do
-    door = Door.new(true, false)
-    expect(door.open).must_equal(nil)
-  end
+
+################### LockedDoor ###################
+  ## cannot be opened
+  ## cannot be closed
+  ## cannot be locked
+  ## can be unlocked
+#09
+    it "Test to see if a locked door, CANNOT be opened " do
+      door = Door.new(true, true)
+      expect(door.open).must_equal(nil)
+          # or raise an argument errror, eventually.
+    end
+##10
+  #   it "Test to see if a locked door, CANNOT closed " do
+  #     door = Door.new(true, true)
+  #     expect(door.close).must_equal()
+          # or raise an argument errror?
+  #   end
+
+##11
+  #   it "Test to see if a locked door, CANNOT locked " do
+  #     door = Door.new(true, true)
+  #     expect(door.locked).must_equal()
+  #   end
+##12
+  #   it "Test to see if a locked door, can be unlocked" do
+  #     door = Door.new(true, true)
+  #     expect(door.unlock).must_equal(nil)
+  #   end
 
 
 end
